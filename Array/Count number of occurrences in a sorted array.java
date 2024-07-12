501121fd6631db669ff8049571a1f6f07b5ac2e5
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-12 09:52:02 +0530

    Add files via upload

---
+++ Array/Count number of occurrences in a sorted array.java
@@ -0,0 +1,19 @@
+class Count
+{
+    static int countOccurrences(int arr[], int n, int x)
+    {
+        int res = 0;
+        for (int i=0; i<n; i++)
+            if (x == arr[i])
+              res++;
+        return res;
+    }
+    
+    public static void main(String args[])
+    {
+        int arr[] = {1, 2, 2, 2, 2, 3, 4, 7 ,8 ,8 };
+        int n = arr.length;
+        int x = 2;
+        System.out.println(countOccurrences(arr, n, x));
+    }
+}
