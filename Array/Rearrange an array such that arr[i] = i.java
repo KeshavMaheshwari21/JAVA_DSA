c82b58f90c3e0bab31235c4967a4605ff50dafe7
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-17 22:27:49 +0530

    Create Rearrange an array such that arr[i] = i.java

---
+++ Array/Rearrange an array such that arr[i] = i.java
@@ -0,0 +1,46 @@
+class Rearrange{
+    // Function to rearrange the array
+    public static void fixArray(int ar[], int n)
+    {
+        int i, j, temp;
+
+        for(i = 0; i < n; i++)
+        {
+            for(j = 0; j < n; j++)
+            {
+                if (ar[j] == i) 
+                {
+                    temp = ar[j];
+                    ar[j] = ar[i];
+                    ar[i] = temp;
+                    break;
+                }
+            }
+        }
+
+        for(i = 0; i < n; i++)
+        {
+            
+            // If not present
+            if (ar[i] != i)
+            {
+                ar[i] = -1;
+            }
+        }
+     
+        // Print the output
+        System.out.println("Array after Rearranging");
+        for(i = 0; i < n; i++) 
+        {
+            System.out.print(ar[i] + " ");
+        }
+    }
+    public static void main(String[] args)
+    {
+        int n, ar[] = { -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 };
+        n = ar.length;
+     
+        // Calling of the function
+        fixArray(ar, n);
+    }
+    }
