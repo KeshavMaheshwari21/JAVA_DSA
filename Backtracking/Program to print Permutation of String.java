12a424aaa4d79a7a68cebf20895c10de1cfbd73d
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-21 17:22:01 +0530

    Create Program to print Permutation of String.java

---
+++ Backtracking/Program to print Permutation of String.java
@@ -0,0 +1,25 @@
+public class recursion
+{
+    public static void printperm(String str , String permutation)
+    {
+        if(str.length() == 0 )
+        {
+            System.out.println(permutation);
+            return;
+        }
+        
+        for( int i = 0 ; i< str.length() ; i++)
+        {
+            char cur = str.charAt(i);
+            
+            String newstring = str.substring(0 ,i) + str.substring(i+1);
+            printperm( newstring , permutation+cur);
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        String str = "ABC";
+        printperm( str , "");
+    }
+}
